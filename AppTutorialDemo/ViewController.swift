54d17d84a031592b7290bdcf35b25039ab40d6df
Appinventiv <appinventiv@apples-macbook-pro-4.local>
2019-01-01 16:56:29 +0530

    Initial Commit

---
+++ AppTutorialDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTutorialDemo
+//
+//  Created by Appinventiv on 01/01/19.
+//  Copyright © 2019 Vikash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
